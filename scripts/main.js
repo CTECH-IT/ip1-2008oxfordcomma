644a60e37e60ab8cdd0619eb0869156f6ec09fac
Dre <2008oxfordcomma@gmail.com>
2021-12-16 17:15:13 -0600

    Created ship

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,45 @@
-// Put your JavaScript here
+const FPS = 30; // frames per second
+const SHIP_SIZE = 30; // ship height in px
+
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+
+var ship = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    r: SHIP_SIZE / 2,
+    a: 90 / 180 * Math.PI, // conver to radians
+}
+
+// set up the game loop
+setInterval(update, 1000 / FPS);
+
+function update() {
+    // draw background
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // draw triangular ship
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = SHIP_SIZE / 20;
+    ctx.beginPath();
+    ctx.moveTo( // nose of the ship
+        ship.x + ship.r * Math.cos(ship.a),
+        ship.y - ship.r * Math.sin(ship.a)
+    );
+    ctx.lineTo( // rear left
+        ship.x - ship.r * (Math.cos(ship.a) + Math.sin(ship.a)),
+        ship.y + ship.r * (Math.sin(ship.a) - Math.cos(ship.a))
+    );
+    ctx.lineTo( // rear right
+        ship.x - ship.r * (Math.cos(ship.a) - Math.sin(ship.a)),
+        ship.y + ship.r * (Math.sin(ship.a) + Math.cos(ship.a))
+    );
+    ctx.closePath();
+    ctx.stroke();
+    // rotate ship
+
+    // move the ship
+
+    // center dot
+}
