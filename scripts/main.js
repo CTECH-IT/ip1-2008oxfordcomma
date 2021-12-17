3fa3330aba0a103d4462051455ed9a50ae3c8ad3
Dre <2008oxfordcomma@gmail.com>
2021-12-17 16:01:38 -0600

    Added Ship movement

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
 const FPS = 30; // frames per second
+const FRICTION = 0.7; // friction coefficient (0 = no friction, 1 = lots of friction)
 const SHIP_SIZE = 30; // ship height in px
+const SHIP_THRUST = 5; // accelerationof the ship in px per second
+const TURN_SPEED = 360; // turn speed in degrees per second
 
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
@@ -9,37 +12,123 @@ var ship = {
     y: canvas.height / 2,
     r: SHIP_SIZE / 2,
     a: 90 / 180 * Math.PI, // conver to radians
+    rot: 0,
+    thrusting: false,
+    thrust: {
+        x: 0,
+        y: 0
+    }
 }
+
+// set up event handlers
+document.addEventListener("keydown", keyDown);
+document.addEventListener("keyup", keyUp);
 
 // set up the game loop
 setInterval(update, 1000 / FPS);
+
+function keyDown(ev) {
+    switch(ev.keyCode) {
+        case 65: // left arrow (rotate left)
+            ship.rot = TURN_SPEED / 180 * Math.PI / FPS;
+            break;
+        case 87: // up arrow (move forward)
+            ship.thrusting = true;
+            break;
+        case 68: // right arrow (rotate right)
+            ship.rot = -TURN_SPEED / 180 * Math.PI / FPS;
+            break;
+    }
+}
+
+function keyUp(ev) {
+    switch(ev.keyCode) {
+        case 65: // left arrow (stop rotating left)
+            ship.rot = 0;
+            break;
+        case 87: // up arrow (stop forward movement)
+        ship.thrusting = false;
+            break;
+        case 68: // right arrow (stop rotating right)
+            ship.rot = 0;
+            break;
+    }
+}
 
 function update() {
     // draw background
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+    // forward movement
+    if (ship.thrusting) {
+        ship.thrust.x -= SHIP_THRUST * Math.cos(ship.a) / FPS;
+        ship.thrust.y -= SHIP_THRUST * Math.sin(ship.a) / FPS;
+
+        // draw the thruster
+        ctx.fillStyle = "red";
+        ctx.strokeStyle = "yellow";
+        ctx.lineWidth = SHIP_SIZE / 10;
+        ctx.beginPath();
+        ctx.moveTo( // rear left
+            ship.x - ship.r * (2 / 3 * Math.cos(ship.a) + 0.5 * Math.sin(ship.a)),
+            ship.y + ship.r * (2 / 3 * Math.sin(ship.a) - 0.5 * Math.cos(ship.a))
+        );
+        ctx.lineTo( // rear center behind the ship
+            ship.x - ship.r * 6 / 3 * Math.cos(ship.a),
+            ship.y + ship.r * 6 / 3 * Math.sin(ship.a)
+        );
+        ctx.lineTo( // rear right
+            ship.x - ship.r * (2 / 3 * Math.cos(ship.a) - 0.5 * Math.sin(ship.a)),
+            ship.y + ship.r * (2 / 3 * Math.sin(ship.a) + 0.5 * Math.cos(ship.a))
+        );
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+    } else {
+        ship.thrust.x -= FRICTION * ship.thrust.x / FPS;
+        ship.thrust.y -= FRICTION * ship.thrust.y / FPS;
+    }
+
     // draw triangular ship
     ctx.strokeStyle = "white";
     ctx.lineWidth = SHIP_SIZE / 20;
     ctx.beginPath();
     ctx.moveTo( // nose of the ship
-        ship.x + ship.r * Math.cos(ship.a),
-        ship.y - ship.r * Math.sin(ship.a)
+        ship.x + 4 / 3 * ship.r * Math.cos(ship.a),
+        ship.y - 4 / 3 * ship.r * Math.sin(ship.a)
     );
     ctx.lineTo( // rear left
-        ship.x - ship.r * (Math.cos(ship.a) + Math.sin(ship.a)),
-        ship.y + ship.r * (Math.sin(ship.a) - Math.cos(ship.a))
+        ship.x - ship.r * (2 / 3 * Math.cos(ship.a) + Math.sin(ship.a)),
+        ship.y + ship.r * (2 / 3 * Math.sin(ship.a) - Math.cos(ship.a))
     );
     ctx.lineTo( // rear right
-        ship.x - ship.r * (Math.cos(ship.a) - Math.sin(ship.a)),
-        ship.y + ship.r * (Math.sin(ship.a) + Math.cos(ship.a))
+        ship.x - ship.r * (2 / 3 * Math.cos(ship.a) - Math.sin(ship.a)),
+        ship.y + ship.r * (2 / 3 *Math.sin(ship.a) + Math.cos(ship.a))
     );
     ctx.closePath();
     ctx.stroke();
+
     // rotate ship
+    ship.a += ship.rot;
 
     // move the ship
+    ship.x -= ship.thrust.x;
+    ship.y += ship.thrust.y;
+
+    // Out of canvas bounds detection
+    if (ship.x < 0 - ship.r) {
+        ship.x = canvas.width + ship.r;
+    } else if (ship.x > canvas.width + ship.r) {
+        ship.x = 0 - ship.r;
+    }
+    if (ship.y < 0 - ship.r) {
+        ship.y = canvas.height + ship.r;
+    } else if (ship.y > canvas.height + ship.r) {
+        ship.y = 0 - ship.r;
+    }  
 
     // center dot
+    // ctx.fillStyle = "red";
+    // ctx.fillRect(ship.x - 1, ship.y - 1, 2, 2);
 }
